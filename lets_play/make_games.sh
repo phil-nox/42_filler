3cc0faf43bbcdd3eabb0e6373d9a0e8899e1b2e8
Wayn Gorold <wgorold@e6r1p6.21-school.ru>
2019-09-19 16:11:26 +0300

    lets_play

--- lets_play/make_games.sh
+++ lets_play/make_games.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #run after make
 
-PATH_R="../resources"
-YOUR_PLY="../build/squid.filler"
-DEBUG_PLY="../build/squid_debug.filler"
+PATH_R="./resources"
+YOUR_PLY="./resources_my/squid.filler"
+DEBUG_PLY="./resources_my/squid_debug.filler"
 
 IDX=0
 CMD_BASE=$PATH_R/filler_vm
 CMD=''
+
+CMD_VISU="cd resources_my ; ../resources/filler_vm"
+YOUR_PLY_VISU="./squid.filler"
 #../resources/filler_vm -p2 ../resources/players/carli.filler -p1 ../build/wgorold.filler -f ../resources/maps/map00
 
 for map in $PATH_R/maps/*; do
@@ -16,23 +19,27 @@ for map in $PATH_R/maps/*; do
     #echo $map
     for player in $PATH_R/players/*; do
         ((IDX+=1))
-        CMD="$CMD_BASE -p1 $YOUR_PLY -p2 $player -f $map -q"
+        CMD="$CMD_BASE -p1 $YOUR_PLY -p2 $player -f $map"
         CMD_D="$CMD_BASE -p1 $DEBUG_PLY -p2 $player -f $map"
+        CMD_V="$CMD_VISU -p1 $YOUR_PLY_VISU -p2 .$player -f .$map"
         echo $IDX
         echo $CMD
-        $($CMD)
+        eval "$CMD -q"
         echo "$CMD -s $(grep seed filler.trace | sed 's/seed = //')" >> filler.trace
         echo "$CMD_D -s $(grep seed filler.trace | sed 's/seed = //')" >> filler.trace
+        echo "$CMD_V -s $(grep seed filler.trace | sed 's/seed = //') | ./filler_visu_vm" >> filler.trace
         mv filler.trace ./result/filler.trace$IDX
 
         ((IDX+=1))
-        CMD="$CMD_BASE -p2 $YOUR_PLY -p1 $player -f $map -q"
+        CMD="$CMD_BASE -p2 $YOUR_PLY -p1 $player -f $map"
         CMD_D="$CMD_BASE -p2 $DEBUG_PLY -p1 $player -f $map"
+        CMD_V="$CMD_VISU -p2 $YOUR_PLY_VISU -p1 .$player -f .$map"
         echo $IDX
         echo $CMD
-        $($CMD)
+        eval "$CMD -q"
         echo "$CMD -s $(grep seed filler.trace | sed 's/seed = //')" >> filler.trace
         echo "$CMD_D -s $(grep seed filler.trace | sed 's/seed = //')"  >> filler.trace
+        echo "$CMD_V -s $(grep seed filler.trace | sed 's/seed = //') | ./filler_visu_vm" >> filler.trace
         mv filler.trace ./result/filler.trace$IDX
     done
 done
